dc0859512c43c64b12836ec31ed8bb11db5899a5
Kiyas <kiiyas@yandex.ru>
2021-08-11 22:32:38 +0500

    methods are separated

--- src/js/__tests__/players.test.js
+++ src/js/__tests__/players.test.js
@@ -1,20 +1,21 @@
 import sortByHealth from '../players';
 
 describe('function sortByHealth should sort players by health', () => {
+  const entryList = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expectedList = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
   test('Should return a list of layers sorted correctly', () => {
-    const entryList = [
-      { name: 'мечник', health: 10 },
-      { name: 'маг', health: 100 },
-      { name: 'лучник', health: 80 },
-    ];
-
-    const expectedList = [
-      { name: 'маг', health: 100 },
-      { name: 'лучник', health: 80 },
-      { name: 'мечник', health: 10 },
-    ];
-
     expect(sortByHealth(entryList)).toEqual(expectedList);
+  });
+  test('Should return a list of layers sorted correctly', () => {
     expect(sortByHealth(entryList)).not.toBe(expectedList);
   });
 });
